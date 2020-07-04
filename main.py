02f22260fa40d7ce0ff1da9bace8a7c47bd918c6
Jonathan Vice <jajvice@gmail.com>
2020-07-04 22:32:14 +0200

    Add memory

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from src.toymachine.machine import Machine
 
 def main():
     machine = Machine()
-    machine.cpu.store("AX", 5)
-    print(machine.dump())
+    machine.cpu.store("AX", 56)
+    machine.cpu.store_in_memory(0, 56)
+
+    print("AX Register:", machine.cpu.load("AX", "d"))
+    print("Memlocation 0:", machine.cpu.load_from_memory(0, return_type="d"))
+
+    machine.cpu.load_next_instruction()
+    print(f"\n{machine.dump()}")
 
 
 if __name__ == "__main__":
--- src/toymachine/machine.py
+++ src/toymachine/machine.py
@@ -17,10 +17,76 @@ def dec2bin(value: int, bits: int) -> List[int]:
         val.append(value % 2)
         value = floor(value / 2)
 
-    for i in range(bits - len(val)):  # Pad the remaining bits
-        val.insert(0, 0)
+    val.extend(0 for _ in range(bits - len(val)))  # Pad the remaining bits
+    val.reverse()
 
     return val
+
+
+class Memory:
+
+    memory_blocks: Dict[int, List[int]]
+
+    def __init__(self, num: int, bytes: int):
+        self.create_memory(num, bytes)
+
+    def create_memory(self, num: int, bytes: int):
+        self.memory_blocks = {}
+
+        for i in range(num):
+            self.memory_blocks[i] = [0 for _ in range(8 * bytes)]
+
+    def dump(self) -> str:
+        dump = "\nMemory Dump\n"
+
+        for addr, block in self.memory_blocks.items():
+            dump += f"{addr}: {block}\n"
+
+        return dump
+
+    def load(
+        self, address: int, length: int = 8, pos: int = 0, return_type: str = "b"
+    ) -> Any:
+        if address not in self.memory_blocks:
+            raise Exception(f"Memory address {address} does not exist")
+
+        mem_length = len(self.memory_blocks[address])
+
+        if (length + pos) > mem_length:
+            raise Exception(
+                f"Memory address {address} is {mem_length} bits long, but {length} bits were requested from position {pos}"
+            )
+
+        value = self.memory_blocks[address][pos : pos + length]
+
+        if return_type == "d":
+            value = bin2dec(value)
+
+        return value
+
+    def store(self, address: int, value: Any, length: int = 8, pos: int = 0) -> None:
+        if address not in self.memory_blocks:
+            raise Exception(f"Memory address {address} does not exist")
+
+        mem_length = len(self.memory_blocks[address])
+
+        if isinstance(value, int):
+            value = dec2bin(value, length)  # Get only the bits needed
+
+        if length < len(value):
+            length = len(value)
+            # ? Maybe raise exception for supplied value greater than bit length here
+
+        if mem_length - pos < length:
+            raise Exception(
+                f"Memory address {address} is {mem_length} bits long, but the value to be stored is {e} bits long from position {pos}"
+            )
+
+        for i in range(len(self.memory_blocks[address])):
+            if i >= length:
+                break
+
+            self.memory_blocks[address][i + pos] = value[i]
 
 
 class CPU:
@@ -28,7 +94,10 @@ class CPU:
 
     registers: Dict[str, List[int]]
 
-    def __init__(self, bits: int = 8):
+    memory: Memory
+
+    def __init__(self, memory: Memory, bits: int = 8):
+        self.memory = memory
         self.bits = bits
         self.create_registers(bits)
 
@@ -38,6 +107,7 @@ class CPU:
         self.registers = {
             "AX": [0 for _ in range(bits)],  # Accumulator
             "CX": [0 for _ in range(bits)],  # Counter
+            "IX": [0 for _ in range(bits)],  # Instruction
             "DX": [0 for _ in range(bits)],  # Data
             "PC": [0 for _ in range(bits)],  # Program Counter
         }
@@ -51,11 +121,11 @@ class CPU:
 
         return regdump
 
-    def load(self, regid: int, return_type: str = "b") -> List[int]:
+    def load(self, regid: str, return_type: str = "b") -> Any:
         """Load a value from a register."""
 
         if regid not in self.registers:
-            raise Exception(f"register {regid} does not exist")
+            raise Exception(f"Register {regid} does not exist")
 
         value = self.registers[regid]
         if return_type == "d":
@@ -63,25 +133,58 @@ class CPU:
 
         return value
 
-    def store(self, regid: int, value: Any) -> None:
+    def load_from_memory(
+        self, address: int, length: int = 8, pos: int = 0, return_type: str = "b"
+    ) -> Any:
+        """Load a value from memory."""
+
+        return self.memory.load(address, length, pos, return_type)
+
+    def store_in_memory(
+        self, address: int, value: Any, length: int = 8, pos: int = 0
+    ) -> None:
+        """Store a value in memory."""
+
+        self.memory.store(address, value, length, pos)
+
+    def store(self, regid: str, value: Any) -> None:
         """Store a value in a register."""
 
         if regid not in self.registers:
-            raise Exception(f"register {regid} does not exist")
+            raise Exception(f"Register {regid} does not exist")
 
         if isinstance(value, int):
             value = dec2bin(value, self.bits)
 
+        if (v := len(value)) > (l := len(self.registers[regid])):
+            raise Exception(
+                f"Register {regid} is {l} bits long, but the value to be stored is {v} bits long"
+            )
+
         self.registers[regid] = value
+
+    def load_next_instruction(self) -> None:
+        """Load the next instruction for execution."""
+
+        next_instruction_location = self.load("PC", "d")
+        next_instruction = self.load_from_memory(next_instruction_location)
+        print("Next instruction:", next_instruction)
+        self.store("IX", next_instruction)
 
 
 class Machine:
     cpu: CPU
 
+    memory: Memory
+
     def __init__(self):
-        self.cpu = CPU()
+        self.memory = Memory(8, 4)  # 8 * 4 bytes = 32 bytes
+        self.cpu = CPU(self.memory)
 
     def dump(self) -> str:
         """Dump the machine state."""
 
-        return self.cpu.dump()
+        dump = self.cpu.dump()
+        dump += self.memory.dump()
+
+        return dump
